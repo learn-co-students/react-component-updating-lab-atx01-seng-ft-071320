8a23c0f703fd2ccaf66919fcb7d90fe7ed16bb22
Ceddyp19 <ceddyp117@gmail.com>
2020-09-16 23:06:47 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-class Timer extends Component {
+class Timer extends PureComponent {
   constructor() {
     super();
     this.timer = React.createRef();
@@ -16,7 +16,7 @@ class Timer extends Component {
     "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate(nextProps, nextState) {  //Added pureComponent as alternative
     if (this.state.time === nextState.time) {
       return false
     }
